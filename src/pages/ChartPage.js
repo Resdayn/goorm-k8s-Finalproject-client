f5797308a6ddba11fc011b6a23798b26462d1740
Juan Gonzalez <jgonzalez@naver.com>
2022-07-18 18:31:53 +0900

    added component

---
+++ src/pages/ChartPage.js
@@ -0,0 +1,6 @@
+
+const ChartPage = () => {
+    return (<h1>ChartPage Component</h1>);
+};
+
+export default ChartPage;
---
+++ src/pages/ListPage.js
@@ -0,0 +1,5 @@
+const ListPage = () => {
+    return (<h1>ListPage Component</h1>);
+};
+
+export default ListPage;
