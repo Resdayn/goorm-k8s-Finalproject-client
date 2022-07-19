582bef28a930b90c1a4ebde13fcd2a265ad980a2
Juan Gonzalez <jgonzalez@naver.com>
2022-07-19 15:04:01 +0900

    added css & login buttons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,28 @@
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
+import styles from "../styles/css/Navigation.module.css";
 
 const Navigation = () => {
-    return (
-        <nav>
-            <ul>
-                <li><h1>Steam DB Clone</h1></li>
-                <li><SearchBar /></li>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/chart">Chart</Link></li>
-                <li><Link to="/list">List</Link></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className={styles.navigationBar}>
+      <div className={styles.logoSection}>
+        <img
+          className={styles.navigationBarLogo}
+          src={require("../assets/img/Kubernetes_logo.png")}
+        ></img>
+        <h1 className={styles.navigationBarTitle}>Price Comparator<span>beta</span></h1>
+        <SearchBar />
+        <Link to="/">Home</Link>
+        <Link to="/chart">Chart</Link>
+        <Link to="/list">List</Link>
+      </div>
+
+      <div>
+        <button className={styles.button}>Login</button>
+        <button className={styles.button}>Register</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
