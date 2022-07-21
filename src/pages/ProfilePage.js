12ee826be47bc6c0929a830ddc23882e9854570c
Juan Gonzalez <jgonzalez@naver.com>
2022-07-21 17:52:03 +0900

    Added ItemPrices.js to ProfilePage route

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,7 +1,13 @@
 import ItemDisplay from "../components/ItemDisplay";
+import ItemPrices from "../components/ItemPrices";
 
 const ProfilePage = () => {
-    return (<ItemDisplay />);
+  return (
+    <section>
+      <ItemDisplay />
+      <ItemPrices />
+    </section>
+  );
 };
 
 export default ProfilePage;
