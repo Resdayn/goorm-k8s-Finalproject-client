ccbe8f1a0bcf8c1e298ac729b4c4feec8aa6f7e9
Juan Gonzalez <jgonzalez@naver.com>
2022-07-21 18:12:47 +0900

    added PriceHistory.js component in the ProfilePage route

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,11 +1,13 @@
 import ItemDisplay from "../components/ItemDisplay";
 import ItemPrices from "../components/ItemPrices";
+import PriceHistory from "../components/PriceHistory";
 
 const ProfilePage = () => {
   return (
     <section>
       <ItemDisplay />
       <ItemPrices />
+      <PriceHistory />
     </section>
   );
 };
