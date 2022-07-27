e9908b6111c7410fc494e1853e8d014bb566ecec
Juan Gonzalez <jgonzalez@naver.com>
2022-07-27 14:27:53 +0900

    removed the dummy data from SearchResults and added it as a prop

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,45 +1,12 @@
 import SearchItem from "./SearchItem";
 import styles from "../styles/css/SearchResults.module.css";
 
-const SearchResults = () => {
-  const dummyData = [
-    {
-      thumbnail: "../assets/img/Kubernetes_logo.png",
-      platform: "Steam",
-      name: "Test Game 1",
-      tags: ["FPS", "RPG"],
-      originalPrice: 30000,
-      discountPrice: 15000,
-      cut: 50,
-      link: "http://steamcommunity.com",
-    },
-    {
-      thumbnail: "../assets/img/Kubernetes_logo.png",
-      platform: "EPIC Store",
-      name: "Test Game 2",
-      tags: ["Strategy", "RPG"],
-      originalPrice: 60000,
-      discountPrice: 15000,
-      cut: 75,
-      link: "http://steamcommunity.com",
-    },
-    {
-      thumbnail: "../assets/img/Kubernetes_logo.png",
-      platform: "GOG",
-      name: "Test Game 3",
-      tags: ["Horror", "FPS"],
-      originalPrice: 10000,
-      discountPrice: 9000,
-      cut: 10,
-      link: "http://steamcommunity.com",
-    },
-  ];
-
+const SearchResults = (props) => {
   return (
     <section>
       <table className={styles.SearchResultsTable}>
         <tbody>
-          {dummyData.map((item) => (
+          {props.results.map((item) => (
             <SearchItem
               key={item.name}
               thumbnail={item.thumbnail}
