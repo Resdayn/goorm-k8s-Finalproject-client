a721aa2e2464a135a7bd01049282801101088ece
Juan Gonzalez <jgonzalez@naver.com>
2022-07-28 10:48:24 +0900

    added a title section in the ChartPage route

--- src/pages/ChartPage.js
+++ src/pages/ChartPage.js
@@ -1,11 +1,15 @@
 import Chart from "../components/Chart";
+import styles from "../styles/css/ChartPage.module.css";
 
 const ChartPage = () => {
   return (
-    <>
-      <h1>ChartPage Component</h1>
+    <section className={styles.ChartPageContainer}>
+      <div className={styles.ChartPageTitleContainer}>
+        <h1 className={styles.ChartPageTitle}>Chart</h1>
+        <p className={styles.ChartPageOnline}><strong>####</strong> Online</p>
+      </div>
       <Chart />
-    </>
+    </section>
   );
 };
 
